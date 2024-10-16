72fe77885c4186367a08449fbc94ec644b757cef
youreachedrahat_wsl <70068895+youreachedrahat@users.noreply.github.com>
2024-10-16 01:07:14 +0000

    week02 Homework

--- code/Week02/homework/Homework1.hs
+++ code/Week02/homework/Homework1.hs
@@ -7,7 +7,7 @@ module Homework1 where
 
 import qualified Plutus.V2.Ledger.Api as PlutusV2
 import           PlutusTx             (compile)
-import           PlutusTx.Prelude     (Bool (..), BuiltinData)
+import           PlutusTx.Prelude     (Bool (..), Eq ((==)), BuiltinData, otherwise, (&&))
 import           Utilities            (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
@@ -16,7 +16,10 @@ import           Utilities            (wrapValidator)
 {-# INLINABLE mkValidator #-}
 -- This should validate if and only if the two Booleans in the redeemer are True!
 mkValidator :: () -> (Bool, Bool) -> PlutusV2.ScriptContext -> Bool
-mkValidator _ _ _ = False
+mkValidator _ (x,y) _ 
+            | x == True = y == True
+            -- | (x == True && y == True) = True
+            | otherwise = False
 
 wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 wrappedVal = wrapValidator mkValidator
--- code/Week02/homework/Homework2.hs
+++ code/Week02/homework/Homework2.hs
@@ -8,10 +8,10 @@
 module Homework2 where
 
 import qualified Plutus.V2.Ledger.Api as PlutusV2
-import           PlutusTx             (unstableMakeIsData)
-import           PlutusTx.Prelude     (Bool, BuiltinData)
-import           Prelude              (undefined)
---import           Utilities            (wrapValidator)
+import           PlutusTx             (unstableMakeIsData, compile)
+import           PlutusTx.Prelude     (Bool, BuiltinData, (/=))
+-- import           Prelude              (undefined)
+import           Utilities            (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
@@ -26,10 +26,10 @@ PlutusTx.unstableMakeIsData ''MyRedeemer
 {-# INLINABLE mkValidator #-}
 -- Create a validator that unlocks the funds if MyRedemeer's flags are different
 mkValidator :: () -> MyRedeemer -> PlutusV2.ScriptContext -> Bool
-mkValidator = undefined
+mkValidator _ r _ = flag1 r /= flag2 r
 
 wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
-wrappedVal = undefined
+wrappedVal = wrapValidator mkValidator
 
 validator :: PlutusV2.Validator
-validator = undefined
+validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| wrappedVal ||])
